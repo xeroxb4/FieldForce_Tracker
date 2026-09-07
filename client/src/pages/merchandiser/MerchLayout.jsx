6382a565e080a4c3a17793e5dc76f5c8df35fb5e
xeroxb4 <xeroxb4@gmail.com>
2026-09-07 06:06:57 +0000

    Fix Link is not defined on OMR/Merch layout

--- client/src/pages/merchandiser/MerchLayout.jsx
+++ client/src/pages/merchandiser/MerchLayout.jsx
@@ -1,4 +1,4 @@
-import { NavLink, Outlet, useNavigate } from 'react-router-dom';
+import { NavLink, Outlet, useNavigate, Link } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
 import { useTheme } from '../../context/ThemeContext';
 import UserAvatar from '../../components/UserAvatar';
